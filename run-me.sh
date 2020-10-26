9745e4854639fe72d87871723e0b788aa81942a0
Cristian Klein <cristian.klein@elastisys.com>
2020-10-26 19:28:16 +0100

    Restrict Tor browser

--- run-me.sh
+++ run-me.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-docker run -it --rm --name tor-browser \
-           -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
-           -e DISPLAY=unix$DISPLAY \
-           cklein/tor-browser
+docker run \
+    -it \
+    --rm \
+    --name tor-browser \
+    -v /tmp/.X11-unix:/tmp/.X11-unix:ro \
+    -e DISPLAY=unix$DISPLAY \
+    --cap-drop=ALL \
+    --cpuset-cpus=0 \
+    cklein/tor-browser
